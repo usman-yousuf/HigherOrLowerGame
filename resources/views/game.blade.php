ffec8db9acbbb8c509d5d2b877899a9714ea08a7
usman-yousuf <uyousaf709@gmail.com>
2023-11-10 12:52:14 +0000

    create new game.blade.php file to display the card

---
+++ resources/views/game.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Higher or Lower Game</title>
+</head>
+<body>
+    <h1>Higher or Lower Game</h1>
+    <p>Current Card: {{ $currentCard->value }} of {{ $currentCard->suit }}</p>
+</body>
+</html>
