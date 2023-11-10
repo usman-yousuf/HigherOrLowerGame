523fdea29b70316522b3c96809a6114d45181e21
usman-yousuf <uyousaf709@gmail.com>
2023-11-10 11:28:16 +0000

    modifiy GameController get the Api Data

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class GameController extends Controller
 {
-    //
+    public function callApi()
+    {
+        $apiUrl = "https://higherorlower-api.netlify.app/json";
+
+        $data = file_get_contents($apiUrl);
+        $response = json_decode($data);
+        dd($response);
+    }
 }
